d5ef1cdd1950e8b0e0b68c542105ac387f40747f
Lody Borgers <hi@lodybo.nl>
2018-12-20 00:01:13 +0100

    Configured styles on a very, very basic level.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,6 @@
-// custom typefaces
-import 'typeface-bungee'
-import 'typeface-roboto-slab'
+// Custom typefaces
+import 'typeface-bungee';
+import 'typeface-roboto-slab';
+
+// Website styles
+import './src/utils/styles/styles.css';
