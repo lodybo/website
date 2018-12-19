81f361a8082a53c6a7b77315bbfc68405da376ce
Lody Borgers <hi@lodybo.nl>
2018-12-19 23:50:13 +0100

    Removed references to typography and such.

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-import { rhythm, scale } from '../utils/typography'
-
 class Layout extends React.Component {
   render() {
     const { location, title, children } = this.props
@@ -13,8 +11,7 @@ class Layout extends React.Component {
       header = (
         <h1
           style={{
-            ...scale(1.5),
-            marginBottom: rhythm(1.5),
+            marginBottom: 0,
             marginTop: 0,
           }}
         >
@@ -34,9 +31,9 @@ class Layout extends React.Component {
       header = (
         <h3
           style={{
-            fontFamily: 'Montserrat, sans-serif',
+            fontFamily: 'Bungee, sans-serif',
             marginTop: 0,
-            marginBottom: rhythm(-1),
+            marginBottom: 0,
           }}
         >
           <Link
@@ -57,8 +54,8 @@ class Layout extends React.Component {
         style={{
           marginLeft: 'auto',
           marginRight: 'auto',
-          maxWidth: rhythm(24),
-          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+          maxWidth: '75%',
+          padding: 15,
         }}
       >
         {header}
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -4,7 +4,6 @@ import { Link, graphql } from 'gatsby'
 import Bio from '../components/bio'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
-import { rhythm, scale } from '../utils/typography'
 
 class BlogPostTemplate extends React.Component {
   render() {
@@ -18,10 +17,9 @@ class BlogPostTemplate extends React.Component {
         <h1>{post.frontmatter.title}</h1>
         <p
           style={{
-            ...scale(-1 / 5),
             display: 'block',
-            marginBottom: rhythm(1),
-            marginTop: rhythm(-1),
+            marginBottom: 0,
+            marginTop: 0,
           }}
         >
           {post.frontmatter.date}
@@ -29,7 +27,7 @@ class BlogPostTemplate extends React.Component {
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
         <hr
           style={{
-            marginBottom: rhythm(1),
+            marginBottom: 0,
           }}
         />
         <Bio />
