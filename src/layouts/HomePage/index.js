9598e970caa7cf8fbda85a1f09613c09d6c26613
Lody Borgers <lodyborgers@gmail.com>
2019-05-07 23:11:06 +0200

    Added info boxes to the homepage

--- src/layouts/HomePage/index.js
+++ src/layouts/HomePage/index.js
@@ -11,21 +11,50 @@ const  HomePageLayout = () => (
       const { author } = data.site.siteMetadata;
 
       return (
-        <div className={ styles.header }>
-          <Image
-            className={ styles.avatar }
-            fixed={ data.avatar.childImageSharp.fixed }
-            alt={ author }
-          />
+        <>
+          <div className={ styles.header }>
+            <Image
+              className={ styles.avatar }
+              fixed={ data.avatar.childImageSharp.fixed }
+              alt={ author }
+            />
 
-          <h1 className={ styles.title }>
-            Hi! I'm <em>Lody</em>, and I'm a front-end developer.
-          </h1>
+            <h1 className={ styles.title }>
+              Hi! I'm <em>Lody</em>, and I'm a front-end developer.
+            </h1>
 
-          <h2 className={ styles.subtitle }>
-            I live in the Netherlands, work at <a href="https://www.taf.nl" target="_blank" rel="noopener noreferrer">TAF</a>, and make a <a href="https://www.themarch.nl" target="_blank" rel="noopener noreferrer">bit of music too</a>.
-          </h2>
-        </div>
+            <h2 className={ styles.subtitle }>
+              I live in the Netherlands, work at <a href="https://www.taf.nl" target="_blank" rel="noopener noreferrer">TAF</a>, and make a <a href="https://www.themarch.nl" target="_blank" rel="noopener noreferrer">bit of music too</a>.
+            </h2>
+          </div>
+
+          <div className={ styles.info }>
+            <div>
+              <p>
+                To see some of the very cool people and organisations I've worked with, or the certificates that I got, take a look at my <a href="https://www.linkedin.com/in/lodybo" target="_blank" rel="noopener nofollower">LinkedIn profile</a>.
+              </p>
+
+              <p>You can also find me on:</p>
+              <ul>
+                <li>
+                  <a href="https://www.twitter.com/lodybo" target="_blank" rel="noopener nofollower">
+                    Twitter
+                  </a>
+                </li>
+
+                <li>
+                  <a href="https://www.github.com/lodybo" target="_blank" rel="noopener nofollower">
+                    GitHub
+                  </a>
+                </li>
+              </ul>
+            </div>
+
+            <div>
+              <em>A form should appear here...</em>
+            </div>
+          </div>
+        </>
       );
     }}
   />
